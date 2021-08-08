c15edb46593dae3064772d4f7a7a33bc6809beb6
e.g.sidorov <e.g.sidorov@tinkoff.ru>
2021-08-08 20:55:24 +0300

    add timer

--- src/app/components/game/components/play/pipe/dual-view-timer/dual-view-timer.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DualViewTimerPipe } from './dual-view-timer.pipe';
-
-describe('DualViewTimerPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DualViewTimerPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
